086151d7e7480e18d4337a0dbbea5dbc8c993cbe
kinggq <18701094030@163.com>
2023-08-25 15:07:10 +0800

    chore: update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,65 @@
 <script setup lang="ts">
+const vTextHoverColor = {
+  mounted(el: HTMLElement, { value = 'black' }) {
+    // 需要给父盒子加相对定位或绝对定位
+    console.log('value', value)
+    const mask = document.createElement('div')
+    const line = document.createElement('div')
+    mask.innerHTML = el.innerHTML
+    mask.style.cssText = `
+      z-index: 9;
+      position: absolute;
+      top: 0;
+      left: 0;
+      color: #fff;
+      background-color: ${value};
+      transition: all 0.35s;
+      text-shadow: initial;
+      -webkit-background-clip: text;
+      clip-path: circle(75% at 50% 50%);
+    `
+    const line_height = el.offsetHeight / 8
+    line.style.cssText = `
+      z-index: 9;
+      position: absolute;
+      width: 0%;
+      height: ${line_height}px;
+      bottom: 0;
+      color: transparent;
+      background-color: ${value};
+      transition: all 0.35s;
+      left: 50%;
+      transform: translate(-50%,5px);
+      border-radius: ${line_height}px;
+    `
+    window.addEventListener('resize', () => {
+      const line_height = el.offsetHeight / 20
+      line.style.height = `${line_height}px`
+      line.style.borderRadius = `${line_height}px`
+    })
 
+    el.appendChild(mask)
+    el.appendChild(line)
+    el.addEventListener('mouseenter', () => {
+      mask.style.clipPath = 'circle(0% at 50% 50%)'
+      line.style.width = '100%'
+    })
+    el.addEventListener('mouseleave', () => {
+      mask.style.clipPath = 'circle(75% at 50% 50%)'
+      line.style.width = '0%'
+    })
+  },
+}
 </script>
 
 <template>
   <header flex items-center justify-between>
     <RouterLink to="/" m-5>
-      <h1 text="20px gray-500" font-bold>
-        King
-      </h1>
+      <div class="TextHoverColor">
+        <h1 v-textHoverColor="'gray'" text="20px gray-500" font-bold>
+          King
+        </h1>
+      </div>
     </RouterLink>
     <nav class="nav">
       <div />
@@ -37,6 +89,13 @@
 </template>
 
 <style scoped>
+.TextHoverColor {
+  position: relative;
+  font-size: 10vw;
+  color: #3498db;
+  cursor: pointer;
+  user-select: none;
+}
 .nav {
   padding: 2rem;
   width: 100%;
